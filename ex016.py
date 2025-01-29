40ecf844b79c668c3d90d3e4d32b7e26b438030e
lory <silvalorrainy100@gmail.com>
2025-01-29 14:02:36 -0300

    exerícios

---
+++ ex016.py
@@ -0,0 +1,6 @@
+#Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção Inteira.
+
+from math import trunc
+
+num = float(input('digite um valor: '))
+print('o valor digitado foi {} e sua porção inteira é {}'.format(num, trunc(num)))
---
+++ ex017.py
@@ -0,0 +1,14 @@
+#Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um triângulo retângulo.
+#Calcule e mostre o comprimento da hipotenusa.
+
+'''co = float(input('qual o comprimento do cateto oposto do triângulo retângulo? '))
+ca = float(input('qual o comprimento do cateto adjacente do triângulo retângulo? '))
+ch = (co ** 2 + ca ** 2) ** (1/2)
+print ('a hipotenusa vai medir {:.2f}'.format(ch))'''
+
+from math import hypot
+
+co = float(input('qual o comprimento do cateto oposto? '))
+ca = float (input('qual o comprimento do cateto adjacente? '))
+ch = hypot(co,ca)
+print('a hipotenusa vai medir {:.2f}'.format(ch))
